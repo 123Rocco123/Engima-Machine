05c585e5d64eaf846f82c5c65087c2f56a4dbb97
123Rocco123 <63969485+123Rocco123@users.noreply.github.com>
2021-06-02 02:43:42 +0100

    Initial Encryption Process
    The initial encryption process of the machine has been completed with this commit.
    
    There is a bug where 2 wrong elements are added to the list when they shouldn't, this has to be fixed.

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import collections
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','t','u','v','w','x','y','z']
 
 #These are some of the orignal rotaters that were used in Engima machines, which were orignaly created in 1922. 
-roter1 = ['D','M','T','W','S','I','L','R','U','Y','Q','N','K','F','E','J','C','A','Z','B','P','G','X','O','H','V']
+rotor1 = ['D','M','T','W','S','I','L','R','U','Y','Q','N','K','F','E','J','C','A','Z','B','P','G','X','O','H','V']
 roter2 = ['H','Q','Z','G','P','J','T','M','O','B','L','N','C','I','F','D','Y','A','W','V','E','U','S','R','K','X']
 roter3 = ['U','Q','N','T','L','S','Z','F','M','R','E','H','D','P','X','K','I','B','V','Y','G','J','C','W','O','A']
 
@@ -29,7 +29,9 @@ rotater1_shift = 0
 rotater2_shift = 0
 rotater3_shift = 0
 
-rot1 = collections.deque(roter1)
+index1 = 0
+
+rot1 = collections.deque(rotor1)
 rot2 = collections.deque(roter2)
 rot3 = collections.deque(roter3)
 
@@ -39,35 +41,49 @@ def roterfunc(rotor, rot):
     #The global statements are used to access the variables that were defined outside of the function. 
     global start, rotater1_shift, rotater2_shift, rotater3_shift
     global rot1, rot2, rot3
+    global index1
     
     #The chosen_roter function is used to determine which roter is currently being used to encrypt the word, and how the roter has to rotate or not. 
     chosen_roter = rotor
 
     #This is the initial positions of the first rotater for the enigma machine. 
     while start == True:
-        roter_pos = int(input("Choose the position of the first roter (1 - 26): "))
-        test = input("Write your first word: ").lower()
-        
-        start = False
+        global test
 
-    #The following line is the rotation that occurs on the initial setup of the roters. 
-    rot1.rotate(roter_pos * -1)
+        if start == True:
+            rotor_pos = int(input("Choose the position of the first roter (1 - 26): "))
+            test = input("Write your first word: ").lower()
+        
+            start = False
+
+            #The following line is the rotation that occurs on the initial setup of the roters. 
+            rot1.rotate(rotor_pos * -1)
 
     #The for loop is used to cycle through the alphabet and see if the letter which is being encrypted matches or not.
         #If it does match, then the index of that letter will be used to be put in the roter, and whichever letter corresponds to that index will be outputted. 
-    for letters in alphabet:
-        if test == letters:
-            index = alphabet.index(letters)
-            test_word.append(rot[index])
+    if rotor == rotor1:
+        for letters in alphabet:
+            if test == letters:
+                index = alphabet.index(letters)
+                test_word.append(rot[index])
 
-            if rotor == roter1:
-                rot1.rotate(-1)
-    
-    print(test_word, rot[-1])
-    
-    if chosen_roter == roter1:
+                if rotor == rotor1:
+                    rot1.rotate(-1)
+    else:
+        for letters1 in alphabet:
+            if letters1 == test_word[len(test_word) - 1].lower():
+                index = alphabet.index(letters1)
+                test_word.append(rot[index])
+
+    if chosen_roter == rotor1:
         rotater1_shift += 1
 
 #The function below is used for the reflector.
+    #The for loop is used to cycle through the pairs in the dictionary, and find the letter which have to be swapped with oneanother.
 def reflector():
-    pass
+    for pairs in relfector1:
+        for letter in test_word:
+            if letter == pairs:
+                test_word.append
+            elif relfector1[pairs] == letter:
+                test_word
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 #Enigma Machine
 
-from functions import roterfunc, roter1, rot1
+from functions import roterfunc, reflector
+from functions import rotor1, roter2, roter3, rot1, rot2, rot3
+from functions import test_word
 
 #This is the change of letter that occurs in the first rotator. 
     #The way that the roter works is by 
 
-roterfunc(roter1, rot1)
-roterfunc(roter2, rot2)
-roterfunc(roter3, rot3)
+roterfunc(rotor1, rot1)
+#print(rotor1)
+#print(rot1)
 
-print(roter1)
-print(rot1)
+roterfunc(roter2, rot2)
+#print(roter2)
+#print(rot2)
+
+roterfunc(roter3, rot3)
+#print(roter3)
+#print(rot3)
+
+#This block of code is used to remove the last two elements in the list which aren't supposed to be there.
+    #This is a temporary solution until a permament solution will be found. 
+for i in range(2):
+    del test_word[-1]
+print(test_word)
+
+#reflector()
